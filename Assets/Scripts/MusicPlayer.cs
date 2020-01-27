cb044dd8711867bed6534a66dcf215420df58659
Mina-projects <Mina_tavangar@yahoo.com>
2020-01-27 16:24:40 +0100

    begining
    Hi

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Invoke("LoadTheScene", 1f);
+        }
+    }
+
+    public void LoadTheScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+
+    [Tooltip("In ms^-1")]
+    [SerializeField]
+    float SpeedFactor = 4f;
+    [SerializeField]
+    float xRange = 0.06f;
+    [SerializeField]
+    float yRange = 0.6f;
+    [SerializeField]
+    float positionPitchFactor = -30f;
+    [SerializeField]
+    float controlPitchFactor = -86f;
+    [SerializeField]
+    float positionYawFactor = -10f;
+    [SerializeField]
+    float controlRollFactor = -40f;
+
+
+    float horizonatlThrow, verticalThrow;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementControl();
+        RotationControl();    
+    }  
+
+
+    public void MovementControl()
+    {
+        horizonatlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // access to the horizontal and vertical axis of this game object
+        verticalThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = horizonatlThrow * SpeedFactor * Time.deltaTime; // controling speed with an offset
+        float yOffset = verticalThrow * SpeedFactor * Time.deltaTime;
+
+        float rawNewxPos = transform.localPosition.x + xOffset; // add the offset to the position, by this you can move the object. 
+        float rawNewyPos = transform.localPosition.y + yOffset;
+
+        float clampXPos = Mathf.Clamp(rawNewxPos, -xRange, xRange); // limiting the movements
+        float clampYPos = Mathf.Clamp(rawNewyPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampXPos, clampYPos, transform.localPosition.z);
+    }
+
+    public void RotationControl()
+    {
+        //pitch
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = verticalThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+        //yaw
+        float yaw = transform.localPosition.x * positionYawFactor;
+
+        //roll
+        float roll = horizonatlThrow * controlRollFactor; 
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+}
